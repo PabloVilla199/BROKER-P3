df2421fb39196afb7c95f875d09113e084c26d1b
PabloVilla199 <pablovillacamanes@gmail.com>
2025-04-02 18:46:35 +0200

    Nueva funcion de ServerImplB

--- src/ServidorB.java
+++ src/ServidorB.java
@@ -4,7 +4,10 @@
  */
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
 
 public interface ServidorB extends Remote {
     String servicioB(String parametro) throws RemoteException;
+    List<String> listarZonasHorarias() throws RemoteException;
+
 }
